00c1b254068a584bdd8c0cd8983bd071ed35599c
AnguiletJonathan <jonathan.anguilet.quaben@etu.univ-st-etienne.fr>
2023-01-18 19:22:12 +0100

    initial commit

--- src/Controller/EportfolioController.php
+++ src/Controller/EportfolioController.php
@@ -8,10 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EportfolioController extends AbstractController
 {
-    #[Route('/eportfolio', name: 'app_eportfolio')]
+    #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('eportfolio/index.html.twig', [
+            'controller_name' => 'EportfolioController',
+        ]);
+    }
+	
+    #[Route('/cv', name: 'cv')]
+    public function cv(): Response
+    {
+        return $this->render('eportfolio/cv.html.twig', [
+            'controller_name' => 'EportfolioController',
+        ]);
+    }
+	
+    #[Route('/eportfolio', name: 'eportfolio')]
+    public function eportfolio(): Response
+    {
+        return $this->render('eportfolio/eportfolio.html.twig', [
+            'controller_name' => 'EportfolioController',
+        ]);
+    }
+	
+    #[Route('/savoir', name: 'savoir_plus')]
+    public function savoir(): Response
+    {
+        return $this->render('eportfolio/savoir.html.twig', [
+            'controller_name' => 'EportfolioController',
+        ]);
+    }
+
+    #[Route('/formulaire_cv', name: 'formulaire_cv')]
+    public function formulaire(): Response
+    {
+        return $this->render('eportfolio/formulaire_cv.html.twig', [
             'controller_name' => 'EportfolioController',
         ]);
     }
